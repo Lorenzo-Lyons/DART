2f3085d4adc1f357c7efb350dbc5a666b33b5e9b
Laranza1 <94372990+Laranza1@users.noreply.github.com>
2024-11-18 12:48:11 +0100

    reorganized in folders a little bit

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lorenzo/OneDrive/PhD/Code/DART/dart_simulator_pkg
-# Build directory: /home/lorenzo/OneDrive/PhD/Code/DART/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
